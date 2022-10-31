bd8ec547c0253a10d732beed94bbca15b3b789fb
laykimleng <bbottra12@gmail.com>
2022-10-31 17:03:22 +0700

    Solidity by Example: Rending and Writing to a state variable

---
+++ contracts/Contants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0x698013BbA6BD97393b2291910b41Ac24736A2F0B;
+    uint public constant MY_UINT = 123;
+}
