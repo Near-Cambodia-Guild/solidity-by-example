0e285b9ecbc1413bd1c777f0da99a56a22db37e8
rozet <pinrozeth@gmail.com>
2022-10-31 17:05:15 +0700

    Solidity

---
+++ contracts/If and Else.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract IfElse {
+    function foo (uint x) public pure returns (uint) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    function ternary (uint _x) public pure returns (uint) {
+        /*
+    function ternary (uint _x) public pure returns (uint) {
+        if (_x < 10) {
+            return 1;
+        }
+        return 2;
+        
+        // Shorthand way to write if / else statemetn
+        // the "?" operator is called the ternary operator
+        */
+
+        return _x < 10 ? 1 : 2;
+    }
+        
+}
