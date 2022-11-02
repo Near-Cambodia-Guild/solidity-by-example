f2092d256bab24b80f04fc8096786b40eb37ba4b
laykimleng <bbottra12@gmail.com>
2022-11-02 14:13:07 +0700

    Solidity

--- contracts/kimleng/Data_Locations_Storage_Memory_Calldata.sol
+++ contracts/kimleng/Data_Locations_Storage_Memory_Calldata.sol
@@ -7,29 +7,28 @@ contract DataLocations {
     struct MyStruct {
         uint foo;
     }
-    mapping(uint => MyStruct) myStructs;
-    
+    mapping(uint => MyStruct) MyStructs;
+
     function f() public {
-        _f(arr, map, myStructs[1]);
+        _f(arr, map, MyStructs[1]);
 
-        MyStruct storage myStruct = myStructs[1];
+        MyStruct storage myStruct = MyStructs[1];
 
-        MyStruct memory myMenStruct = MyStruct(0);   
+        MyStruct memory myMemStruct = MyStruct(0);
     }
 
-   function _f(
-    uint[] storage _arr,
-    mapping(uint => address) storage _map,
-    MyStruct storage _myStruct
-   ) internal {
-    
-   }
+    function _f(
+        uint[] storage _arr,
+        mapping(uint => address) storage _map,
+        MyStruct storage _myStruct
+    ) internal {
 
-   function g(uint[] memory _arr) public returns (uint[] memory) {
-    
-   }
+    }
+    function g(uint[] memory _arr) public returns (uint[] memory) {
 
-   function h(uint[] calldata _arr) external {
+    }
 
-   }
+    function h(uint[] calldata _arr) external {
+
+    }
 }
--- contracts/kimleng/Enum_above.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-// pragma solidity ^0.8.13;
-
-// import "./EnumDeclaration.sol";
-
-// contract Enum {
-//     Status public status;
-// }
-
