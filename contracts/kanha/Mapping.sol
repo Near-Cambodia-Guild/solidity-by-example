fe43987c906fab4fe570ac216ac7e6a610e6dfa5
kanha <muonkanharoth@gmail.com>
2022-11-02 17:30:47 +0700

    Solidity

---
+++ contracts/kanha/Mapping.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+
+//  contract Mappng{
+//     mapping (address=> uint )public myMap;
+
+//     function get (address _addr)public view returns (uint){
+
+//         return myMap[_addr];
+
+
+//     }
+
+//     function set (address _addr, uint  _i)public{
+//          myMap[_addr]= _i;
+
+
+//     }
+
+//     function remove(address _addr)public{
+
+//         delete myMap[_addr];
+//     }
+//  }
+ 
+
+//  contract NestMapping {
+
+//     mapping (address=>mapping(uint=>bool)) public nested;
+
+//     function get (address _addrl, uint _i )public view returns(bool){
+
+//         return nested [_addrl][_i];
+//     }
+
+//     function set (
+        
+//         address _addrl,
+//         uint _i,
+
+//         bool _boo 
+    
+    
+//     )public{
+
+//         nested[_addrl] [_i]= _boo;
+
+
+//     }
+
+//     function remove(address _addrl, uint _i)public{
+
+
+//             delete nested [_addrl][_i];
+//     }
+
+ 
+//  }
