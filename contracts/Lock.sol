8729e45b4656d8ae6b82e8277e9c9292de421549
sammang <masamnang6@gmail.com>
2022-11-06 11:02:31 +0700

    Samnang

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -263,37 +263,37 @@ pragma solidity ^0.8.9;
 //     }
 // }
 
-contract Structs{
-    struct Car {
-        string model;
-        uint year;
-        address owner;
-    }
+// contract Structs{
+//     struct Car {
+//         string model;
+//         uint year;
+//         address owner;
+//     }
 
-    Car public car;
-    Car[] public cars;
-    mapping(address => Car[]) public carsByOwner;
+//     Car public car;
+//     Car[] public cars;
+//     mapping(address => Car[]) public carsByOwner;
 
-    function examples() external {
-        Car memory toyota = Car("Toyota",1990,msg.sender);
-        Car memory lambo = Car({year: 1980, model:"Lamborghini",owner: msg.sender});
-        Car memory tesla;
-        tesla.model="Tesla";
-        tesla.year = 2010;
-        tesla.owner = msg.sender;
+//     function examples() external {
+//         Car memory toyota = Car("Toyota",1990,msg.sender);
+//         Car memory lambo = Car({year: 1980, model:"Lamborghini",owner: msg.sender});
+//         Car memory tesla;
+//         tesla.model="Tesla";
+//         tesla.year = 2010;
+//         tesla.owner = msg.sender;
 
-        cars.push(toyota);
-        cars.push(lambo);
-        cars.push(tesla);
+//         cars.push(toyota);
+//         cars.push(lambo);
+//         cars.push(tesla);
 
-        cars.push(Car("Ferrari",2020,msg.sender));
+//         cars.push(Car("Ferrari",2020,msg.sender));
 
-        Car storage _car = cars[0];
-        // _car.model;
-        // _car. owner;
-        _car.year = 1999;
-        delete _car.owner;
+//         Car storage _car = cars[0];
+//         // _car.model;
+//         // _car. owner;
+//         _car.year = 1999;
+//         delete _car.owner;
 
-        delete cars[1];
-    }
-}
+//         delete cars[1];
+//     }
+// }
